e384153d548b06011dd8bf4c20f7a45895f0dd03
Evg <budo@narod.ru>
2022-08-26 18:58:48 +0300

    DEV: Подготовим дизайн для изменений
    Нам понадобится меню (боковая колонка) в будущем...

---
+++ config/general.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Главные, функциональные настройки сайта
+ * Main, functional site settings 
+ */
+
+return [
+
+    'name'          =>  'LibArea',
+
+    'logo_big'      => '<img class="logo" alt="LibArea" src="/assets/images/LibArea_limpid.png">',
+    'logo_small'    => '<a title="LibArea" href="/">LibArea</a>',
+    'img_home'      => '<img class="w-100" src="/assets/images/libarea-home.jpg" alt="LibArea">',
+      
+    'lang'          => ['ru', 'en'],
+    'default_lang'  => ['ru'],
+
+    // Email of the site administration
+    // Email администрации сайта
+    'email'         => '***@***.ru',
+];
--- resources/views/index.php
+++ resources/views/index.php
@@ -5,7 +5,11 @@
       <?= $help; ?>.
   </div>  
 </div>  
-
+<div class="box-flex">
+  <div class="home-img">
+    <?= config('general.img_home'); ?>
+  </div>
+</div>
 <div class="box-flex">
   <a class="button-large" href="<?= getUrlByName('welcome', ['lang' => 'ru']); ?>">Русский</a>
   <a class="button-large" href="<?= getUrlByName('welcome', ['lang' => 'en']); ?>">English</a>
